a0699ae736ea6144cdfb8400403118d0a0b33a4b
fa955688-commits <fa955688@gmail.com>
2026-06-10 11:14:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 require("./bot.js");
+const { Client, GatewayIntentBits } = require('discord.js');
 
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent, // এটি অবশ্যই থাকতে হবে
+        GatewayIntentBits.GuildMembers
+    ]
+});
